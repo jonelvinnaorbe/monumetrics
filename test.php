4406cbfd9c00d9a2befa55d7af69704641bb95ce
Jon <jonelvinnaorbe@gmail.com>
2021-07-16 15:12:11 +0800

    v1.1

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
  * Plugin Name: Test Plugin
  * Plugin URI: http://www.mywebsite.com/my-first-plugin
  * Description: The very first plugin that I have ever created.
- * Version: 1.0
+ * Version: 1.1
  * Author: Your Name
  * Author URI: http://www.mywebsite.com
  */
