5750e5c7498eb3fa755ea34a71a6ec226ba5f602
Jon <jonelvinnaorbe@gmail.com>
2021-07-16 15:01:43 +0800

    Initial

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Test Plugin
+ * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: The very first plugin that I have ever created.
+ * Version: 1.0
+ * Author: Your Name
+ * Author URI: http://www.mywebsite.com
+ */
+
+echo 'testing here';
+
+// require 'plugin-update-checker/plugin-update-checker.php';
+// $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+// 	'https://github.com/user-name/repo-name/',
+// 	__FILE__,
+// 	'unique-plugin-or-theme-slug'
+// );
